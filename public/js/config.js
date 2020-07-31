e59a093cc5ce903d6591138415b62bd2a035e800
Eagle19243 <eagle19243@gmail.com>
2020-07-31 05:20:02 +0300

    update status against celery state

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,6 @@
 const URL_API   = 'http://192.241.129.79:8093';
 // const URL_API   = 'http://127.0.0.1:5000';
-const RUNNING   = 'PENDING';
-const FINISHED  = 'Task Completed';
+const PENDING   = 'PENDING'
+const RUNNING   = 'PROGRESS';
+const FINISHED  = 'SUCCESS';
 const FAILURE   = 'FAILURE';
