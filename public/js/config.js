aa794826de24236f2b08c6eee20ea5cd6a872c1f
Eagle19243 <eagle19243@gmail.com>
2020-09-07 05:26:10 +0300

    update server url

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
-// const URL_API   = 'http://192.241.129.79:8093';
-const URL_API   = 'http://127.0.0.1:5000';
+const URL_API   = 'http://192.241.129.79:8093';
+// const URL_API   = 'http://127.0.0.1:5000';
 const PROGRESS  = 'PROGRESS';
 const FINISHED  = 'FINISHED';
